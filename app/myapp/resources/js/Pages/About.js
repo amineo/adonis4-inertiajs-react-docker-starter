83fdf80c48cfd1ef6a7be7e855885d231e8b4177
Anthony Mineo <anthonymineo@gmail.com>
2020-03-25 17:18:57 -0400

    cleaning

--- app/myapp/resources/js/Pages/About.js
+++ app/myapp/resources/js/Pages/About.js
@@ -1,18 +1,12 @@
-/** @jsx jsx */
 import React from 'react'
-
-import tw from 'twin.macro'
-import { jsx } from "@emotion/core";
-
 import Layout from '@/Shared/Layout'
-
 
 export default function About(props) {
   return (
     <Layout>
       <h1 className="text-5xl">{props.welcome}</h1>
 
-      <p className="font-sans">It works!</p>
+      <p className="font-sans">Hey, it works!</p>
     </Layout>
   )
 }
